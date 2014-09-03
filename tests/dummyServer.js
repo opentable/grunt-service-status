efbae110c0ece28333461fd8b5229f4f34dd3e8a
Andy Royle <ajroyle@gmail.com>
2014-09-03 14:54:06 +0100

    use async.eachSeries instead of request-sync craziness

--- tests/dummyServer.js
+++ tests/dummyServer.js
@@ -2,31 +2,31 @@ var http = require("http"),
     fs = require("fs"),
     serviceStatus = {
         'success': {
-            'my-test-monitor': { 
-                  "status": "Ok", 
-                  "monitorname": "my-test-monitor", 
-                  "response": 200, 
-                  "time": 1234 
+            'my-test-monitor': {
+                  "status": "Ok",
+                  "monitorname": "my-test-monitor",
+                  "response": 200,
+                  "time": 1234
             },
-            'my-test-monitor-2': { 
-                  "status": "Ok", 
-                  "monitorname": "my-test-monitor-2", 
-                  "response": 200, 
-                  "time": 1234 
+            'my-test-monitor-2': {
+                  "status": "Ok",
+                  "monitorname": "my-test-monitor-2",
+                  "response": 200,
+                  "time": 1234
             }
         },
         'warmUp': {
-            'my-test-monitor': { 
-                "status": "Ok", 
-                "monitorname": "my-test-monitor", 
-                "response": 200, 
-                "time": 1234 
+            'my-test-monitor': {
+                "status": "Ok",
+                "monitorname": "my-test-monitor",
+                "response": 200,
+                "time": 1234
             },
-            'my-test-monitor-2': { 
-                "status": "Failed", 
-                "monitorname": "my-test-monitor-2", 
-                "response": 502, 
-                "time": 1234 
+            'my-test-monitor-2': {
+                "status": "Failed",
+                "monitorname": "my-test-monitor-2",
+                "response": 502,
+                "time": 1234
             }
         },
         'altWarmUp': {
@@ -38,17 +38,17 @@ var http = require("http"),
             },
         },
         'case-sensitive': {
-            'my-test-monitor': { 
-                 "Status": "OK", 
-                 "MonitorName": "my-test-monitor", 
-                 "response": 200, 
+            'my-test-monitor': {
+                 "Status": "OK",
+                 "MonitorName": "my-test-monitor",
+                 "response": 200,
                  "time": 1234
              },
-            'my-test-monitor-2': { 
-                 "Status": "Ok", 
-                 "monitorname": "my-test-monitor-2", 
-                 "response": 200, 
-                 "time": 1234 
+            'my-test-monitor-2': {
+                 "Status": "Ok",
+                 "monitorname": "my-test-monitor-2",
+                 "response": 200,
+                 "time": 1234
              }
         }
     };
@@ -61,7 +61,7 @@ var server = http.createServer(function(request, response) {
     }
 
     var bits = request.url.split('/');
-    var result = serviceStatus[bits[1]]
+    var result = serviceStatus[bits[1]];
     result = bits[2] ? result[bits[2]] : result;
 
     if(bits[1] === 'warmUp' && bits[2]){
